cf8192498353bf402699aa0dec3dd5223682b5ae
yusufms48 <misha.yusufov48@gmail.com>
2026-05-21 20:48:26 +0400

    feat: add SASS styles with variables and mixins

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
 import App from "./App.jsx";
 import "./index.css";
+import "./styles/main.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
